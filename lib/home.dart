c7e355617840ed791cf8bd1b1758b4806161ad7a
vctorr1 <128295802+vctorr1@users.noreply.github.com>
2024-04-10 21:21:41 +0200

    Creando pagina de inicio

---
+++ lib/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('i am a stateful'),
+    );
+  }
+}
