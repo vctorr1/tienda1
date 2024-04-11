f28cc6137ed4d39e36e0a420fa09a26a137853dd
vctorr1 <128295802+vctorr1@users.noreply.github.com>
2024-04-11 09:15:29 +0200

    interfaz

--- lib/main.dart
+++ lib/main.dart
@@ -28,8 +28,47 @@ class _MyHomePageState extends State<HomePage> {
               color: Colors.white,
             ),
             onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.shopping_cart,
+              color: Colors.white,
+            ),
+            onPressed: () {},
           )
         ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          //Creamos lista de widgets
+          children: <Widget>[
+            //Cabecera
+            UserAccountsDrawerHeader(
+              accountName: Text('nombre ejemplo'),
+              accountEmail: Text('ejemplo@email.com'),
+              //Foto de perfil
+              currentAccountPicture: GestureDetector(
+                child: CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  child: Icon(
+                    Icons.person,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              decoration: BoxDecoration(color: Colors.red),
+            ),
+
+            //Cuerpo
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Inicio'),
+                leading: Icon(Icons.home),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
